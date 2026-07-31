a102ebe3e1ddac401eb287d01da22e799ae6e701
muzammil <mhmdmuzammilpk@gmail.com>
2026-08-01 00:04:05 +0530

    feat: create base template and homepage routing

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):#if someone visit local host django will executes this function
+    return render(request, "core/home.html")#tells Django :Open this HTML page and send it back to the browser.
+
