12c8ef09f8c3f4882f7d94a2fab044e1294f9220
muzammil <mhmdmuzammilpk@gmail.com>
2026-07-31 23:12:10 +0530

    feat: create core application architecture

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.core.apps.CoreConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
